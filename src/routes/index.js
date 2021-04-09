14c4e92e2af94d09e684a1030613c9ba8069367f
xinnywinne <x.wang.tju2010@gmail.com>
2021-04-08 21:36:51 -0700

    update quiz-app function and unit test

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,24 +1,66 @@
 const quizzes = require('../../data/quizzes.json');
+const QUESTION = "questions";
+const ANSWER = "answer";
+const ANSWERS = "answers";
 
 /**
  * Returns a list of quizzes with titles and IDs
  */
-async function getQuizzes(req, res, next) {
-  // TODO: Your code goes here
+function getQuizzes(req, res, next) {
+  const ret = Object.values(quizzes).map(quiz => {
+    return {
+      id: quiz.id,
+      title: quiz.title
+    };
+  });
+  return res.json(ret);
 }
 
 /** 
  * Returns quiz data for the given ID, omitting the answers
  */
-async function getQuiz(req, res, next) {
-  // TODO: Your code goes here
+function getQuiz(req, res, next) {
+  const foundQuiz = Object.values(quizzes).find(quiz => quiz.id === req.params.id );
+  if (foundQuiz) {
+    //deep copy foundQuiz to returnQuiz
+    let returnQuiz = JSON.parse(JSON.stringify(foundQuiz));
+    returnQuiz[QUESTION].forEach(question => {
+      delete question[ANSWER]
+    });
+    res.json(returnQuiz);
+  } else {
+    res.status(404).json({
+      msg: `Quiz ${req.params.id} not exist`
+    });
+  }
 }
 
 /**
  * Handles a quiz submission and returns a graded result
  */
-async function postQuiz(req, res, next) {
-  // TODO: Your code goes here
+function postQuiz(req, res, next) {
+  let questionRes = {};
+  let correctNum = 0;
+  let incorrectNum = 0;
+  quizzes[req.params.id][QUESTION].forEach(question => {
+    const found = req.body[ANSWERS].hasOwnProperty(question.id);
+    //Compares the answers only when the request question.id exists in quizzes.
+    if (found) {
+      if (req.body[ANSWERS][question.id] === question.answer) {
+        correctNum++;
+        questionRes[question.id] = "True";
+      } else {
+        incorrectNum++;
+        questionRes[question.id] = "False";
+      }
+    }
+  });
+  const quizRes = {
+    "correct": correctNum,
+    "incorrect": incorrectNum,
+    "questions": questionRes
+  };
+  res.json(quizRes);
 }
 
 module.exports = {
--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,29 +1,123 @@
 const {getQuizzes, getQuiz, postQuiz} = require('../routes');
 
+const mockRequest = (id, body) => {
+  return {
+    params: {
+      id
+    },
+    body
+  };
+};
+const mockResponse = () => {
+  const res = {};
+  res.status = jest.fn().mockReturnValue(res);
+  res.json = jest.fn().mockReturnValue(res);
+  return res;
+}
+
 describe('API', () => {
   describe('getQuizzes', () => {
     it('returns a list of quizzes', () => {
-      // TODO: Your code goes here
-      expect(false).toBeTruthy();
+      let req = mockRequest();
+      let res = mockResponse();
+      getQuizzes(req, res);
+      expect(res.json).toHaveBeenCalledWith([{
+        "id": "math",
+        "title": "Basic Math Quiz",
+      }, {
+        "id": "english",
+        "title": "Basic English Quiz",
+      }]);
     });
   });
 
   describe('getQuiz', () => {
     it('returns the data for a quiz', () => {
-      // TODO: Your code goes here
-      expect(false).toBeTruthy();
+      let req = mockRequest("math");
+      let res = mockResponse();
+      getQuiz(req, res);
+      expect(res.json).toHaveBeenCalledWith({
+        "id": "math",
+        "title": "Basic Math Quiz",
+        "questions": [{
+          "id": "question_1",
+          "text": "What is 1 + 1?",
+          "options": ["1", "2", "3"],
+        }, {
+          "id": "question_2",
+          "text": "True or false: 2 + 2 = 4",
+          "options": ["True", "False"],
+        }, {
+          "id": "question_3",
+          "text": "What is the square root of 49",
+          "options": ["4", "6", "7"],
+        }]
+      });
+      req = mockRequest("english");
+      res = mockResponse();
+      getQuiz(req, res);
+      expect(res.json).toHaveBeenCalledWith({
+        "id": "english",
+        "title": "Basic English Quiz",
+        "questions": [{
+          "id": "question_1",
+          "text": "True or false: 'New York City' is a proper noun",
+          "options": ["True", "False"],
+        }, {
+          "id": "question_2",
+          "text": "What's wrong with this sentence? 'Sitting on the park bench, the sun set slowly behind the mountains.'",
+          "options": ["Run-on sentence", "Dangling participle", "Comma splice"],
+        }, {
+          "id": "question_3",
+          "text": "Which of the following is a possessive pronoun?",
+          "options": ["they're", "their", "there"],
+        }]
+      });
     });
-
     it('returns a 404 if the quiz cannot be found', () => {
-      // TODO: Your code goes here
-      expect(false).toBeTruthy();
+      let req = mockRequest("music");
+      let res = mockResponse();
+      getQuiz(req, res);
+      expect(res.status).toHaveBeenCalledWith(404);
     });
   });
 
   describe('postQuiz', () => {
-    it('returns the correct grades for the quiz', () => {
-      // TODO: Your code goes here
-      expect(false).toBeTruthy();
+    it('returns the correct grades for the quiz',  () => {
+      let req = mockRequest("math", {
+        "answers": {
+          "question_1": "2",
+          "question_2": "True",
+          "question_3": "7"
+        }
+      });
+      let res = mockResponse();
+      postQuiz(req, res);
+      expect(res.json).toHaveBeenCalledWith({
+        "correct": 3,
+        "incorrect": 0,
+        "questions": {
+          "question_1": "True",
+          "question_2": "True",
+          "question_3": "True"
+        }
+      });
+      req = mockRequest("math", {
+        "answers": {
+          "question_1": "1",
+          "question_2": "False"
+        }
+      });
+      res = mockResponse();
+      postQuiz(req, res);
+      expect(res.json).toHaveBeenCalledWith({
+        "correct": 0,
+        "incorrect": 2,
+        "questions": {
+          "question_1": "False",
+          "question_2": "False"
+        }
+      });
     });
   });
 });
